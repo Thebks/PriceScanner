0faa5068872d353deb1689fbeb13c311d6e106c6
Thebks <ichbks@gmail.com>
2023-10-11 22:11:57 +0500

    carousel

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -1,25 +1,33 @@
+"use client"
+
 import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/image';
+
+
+const heroImages = [
+    {imgUrl: 'assets/images/hero-1.svg', alt:"watch"},
+    {imgUrl: 'assets/images/hero-2.svg', alt:"bag"},
+    {imgUrl: 'assets/images/hero-3.svg', alt:"Lamp"},
+    {imgUrl: 'assets/images/hero-4.svg', alt:"air fryer"},
+    {imgUrl: 'assets/images/hero-5.svg', alt:"chair"},
+]
 
 const HeroCarousel = () => {
   return (
-    <>
-        <Carousel>
-            <div>
-                <img src="assets/1.jpeg" />
-                <p className="legend">Legend 1</p>
-            </div>
-            <div>
-                <img src="assets/2.jpeg" />
-                <p className="legend">Legend 2</p>
-            </div>
-            <div>
-                <img src="assets/3.jpeg" />
-                <p className="legend">Legend 3</p>
-            </div>
+    <div className="hero-carousel">
+        <Carousel showThumbs={false} autoPlay infiniteLoop interval={2000} showArrows={false} showStatus={false}>
+           {heroImages.map((image) => (
+            <Image src={image.imgUrl} alt={image.alt} key={image.alt} className="object-contain" width={484} height={484} />
+           ))}
         </Carousel>
-    </>
+        
+        <Image 
+          src="assets/icons/hand-drawn-arrow.svg" 
+          alt="arrow" width={175} height={175} 
+          className="max-xl:hidden absolute -left-[15%] bottom-0 z-0"  />
+    </div>
   )
 }
 
